942b8ab047b177266d192949d99822a1c9325205
Francisco Soares <fmssn@cin.ufpe.br>
2024-08-26 15:54:55 -0300

    Initial Commit

---
+++ TribosTarde/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TribosTarde
+//
+//  Created by Francisco Miranda Soares on 26/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TribosTarde/TribosTardeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TribosTardeApp.swift
+//  TribosTarde
+//
+//  Created by Francisco Miranda Soares on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TribosTardeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
